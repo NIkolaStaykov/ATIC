9831cf452866c978d1dee782e01e8351a482995e
NStaykov <ns@datamachineintelligence.eu>
2025-07-27 22:33:44 +0200

    added a null controller

--- controller.py
+++ controller.py
@@ -4,7 +4,7 @@ import sys
 from enum import Enum
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
-ControllerType = Enum('ControllerType', 'RANDOM ADVERSARIAL')
+ControllerType = Enum('ControllerType', 'RANDOM ADVERSARIAL NULL')
 
 class SensitivityEstimator:
     """Kalman Filter implementation"""
@@ -122,6 +122,8 @@ class Controller:
                 self.control_input = control_input_unclipped / max(control_input_unclipped)
             elif self.controller_type == ControllerType.RANDOM:
                 self.control_input = np.random.rand(self.num_users)
+            elif self.controller_type == ControllerType.NULL:
+                self.control_input = np.zeros(self.num_users)
             else:
                 raise ValueError(f"Unknown ControllerType: {self.controller_type}")
             
